850123235a0732adf2f22e2d2ec567d4ffa66b68
Willy Candra <willycandraappleid@gmail.com>
2024-11-02 10:12:58 +0800

    index, display, product, css, functionality except for add to cart and user registration done

--- index.js
+++ index.js
@@ -26,6 +26,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const category = await axios.get(apiUrl+"/products/categories");
 const testimonyUrl="./JSON/testimonial.json"
 
+
+//display
 app.get("/",async(req,res)=>{
     try {
         const result = await axios.get(apiUrl+"/products");
@@ -40,6 +42,13 @@ app.get("/aboutUs",async(req,res)=>{
     const result = await axios.get(apiUrl+"/products");
         //const category = getUniqueCategory(result.data);
         res.render("aboutus.ejs",{data:result.data, category:category.data});
+})
+
+
+//product
+app.get("/product",async(req,res)=>{
+    let result = await axios.get(apiUrl+"/products");
+    res.render("display.ejs",{data:result.data, category:category.data});
 })
 
 app.post("/product",async(req,res)=>{
@@ -68,12 +77,12 @@ app.post("/product",async(req,res)=>{
         }
         
         result = await axios.get(apiUrl+"/products"+displayed);
-        console.log("req.body.choice :"+req.body.choice+", displayed :"+displayed);
-        console.log(category.data);
-        console.log(result.data);
+        //console.log("req.body.choice :"+req.body.choice+", displayed :"+displayed);
+        //console.log(category.data);
+        //console.log(result.data);
         res.render("display.ejs",{data:result.data, category:category.data});
     } catch (error) {
-        console.log("req.body.choice :"+req.body.choice);
+        //console.log("req.body.choice :"+req.body.choice);
         console.log("error : ",error);
         res.render("display.ejs", {data:error.response});
     }
@@ -84,11 +93,6 @@ app.get("/input",(req,res)=>{
     res.render("input.ejs",{category:category.data, head:headingText});
 }
 )
-
-app.get("/login",async(req,res)=>{
-    console.log(category.data);
-    res.render("login.ejs",{category:category.data});
-})
 
 app.post("/submit",upload.single('image'),async(req,res)=>{
     const postData= await axios.post(apiUrl+"/products",{ data:{
@@ -107,6 +111,68 @@ app.post("/detail",async(req,res)=>{
     console.log(data);
     res.render("details.ejs",{data:data.data, category:category.data});
 })
+
+
+//user
+app.get("/login",async(req,res)=>{
+    let txt="Please Log In"
+    res.render("login.ejs",{category:category.data, text:txt});
+})
+
+app.post("/login",async(req,res)=>{
+    const user = await axios.get(apiUrl+"/users")
+    let username=req.body.username;
+    let password=req.body.password;
+    let auth=false;
+    let index=-1;
+    let txt="Please Log In";
+    let name;
+    //console.log(user.data.id);
+    txt=checkUsernameandPassword(username,password,user.data);
+    res.render("login.ejs",{txt:txt, category:category.data});
+
+    /*for(var i=0;i<user.data.length;i++)
+    {
+        if(username===user.data[i].username)
+        {
+            if(password===user.data[i].password){
+                auth=true;
+                index=i;
+                console.log("login successfully to user :"+user.data[i].name.firstname+" "+user.data[i].name.lastname)
+            }else{
+                console.log("incorrect password");
+                txt="Incorrect Password";
+                res.render("login.ejs",{category:category.data, text:txt})
+            }
+        }else{
+            console
+        }
+    }*/
+});
+
+function checkUsernameandPassword(user,password,data){
+    let result;
+    for(var i=0;i<data.length;i++)
+    {
+        if(user===data[i].username)
+        {
+            console.log("checking user "+user+" to "+ data[i].username);
+            if(password===data[i].password)
+            {
+                //console.log("checking pass "+password+" to "+ data[i].password);
+                result=`Welcome ${data[i].name.firstname} ${data[i].name.lastname}`;
+                break;
+            }else{
+                //console.log("wrong password");
+                result="Wrong Password"
+                break;
+            }
+        }else{
+            result="No User Found!"
+        }
+    }
+    return result;
+}
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
